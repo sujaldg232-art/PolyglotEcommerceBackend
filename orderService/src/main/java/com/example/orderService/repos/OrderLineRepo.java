68c22661af7dcd3f6daa13ac1e444252893ba286
Sujal <257596311+sujaldg232-art@users.noreply.github.com>
2026-07-15 10:10:05 +0530

    Added A new Sql Query to OrderRepository

--- orderService/src/main/java/com/example/orderService/repos/OrderLineRepo.java
+++ orderService/src/main/java/com/example/orderService/repos/OrderLineRepo.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderLineRepo extends JpaRepository<OrderLine,Long> {
 
+
 }
--- orderService/src/main/java/com/example/orderService/repos/OrderRepo.java
+++ orderService/src/main/java/com/example/orderService/repos/OrderRepo.java
@@ -2,13 +2,18 @@ package com.example.orderService.repos;
 
 import com.example.orderService.entities.OrderData;
 
+import com.example.orderService.entities.OrderLine;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface OrderRepo extends JpaRepository<OrderData, UUID> {
 
+    @Query("select p.orderLines from OrderData p where p = :orderData")
+    List<OrderLine> fetchAllTheOrderLine(OrderData orderData);
 
     Optional<OrderData> findByBuyerId(UUID uuid);
 }
